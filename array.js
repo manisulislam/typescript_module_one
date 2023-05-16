719b28b30dc3d6615a177bc0eacf53b0921cda23
anisul islam <anisulislam980340@gmail.com>
2023-05-16 16:03:20 +0600

    typescript array is completed

---
+++ array.js
@@ -0,0 +1,6 @@
+var superheros = [];
+var heroPower = [];
+var allUser = [];
+superheros.push("anis");
+heroPower.push(1);
+allUser.push({ name: "", isActive: true });
---
+++ array.ts
@@ -0,0 +1,13 @@
+const superheros: string[]=[]
+const heroPower: Array<number>=[]
+
+type User={
+    name: string;
+    isActive: boolean;
+}
+
+const allUser: User[]=[];
+
+superheros.push("anis");
+heroPower.push(1);
+allUser.push({name: "", isActive: true})
