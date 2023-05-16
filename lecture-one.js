b5288af792a20f927843ff66872a416d7fc72658
anisul islam <anisulislam980340@gmail.com>
2023-05-16 09:03:29 +0600

    typescript introduction

---
+++ lecture-one.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "hello world";
+// number
+var userId = 12563;
+//boolean
+var isLoggedIn = true;
---
+++ lecture-one.ts
@@ -0,0 +1,8 @@
+let greetings: string= "hello world"
+// number
+let userId: number=12563
+
+
+//boolean
+let isLoggedIn: boolean= true
+export {}
